5fd42548a06493d80284eee8d4389de8cbe02cf2
BalajiPrasadS <balaji143rko@gmail.com>
2024-02-15 08:51:17 +0530

    First commit

---
+++ src/test/java/day1/JavaBasics.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class JavaBasics
+//Who's using Chrome?
+//With Chrome profiles you can separate all your Chrome stuff. Create
+{
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+
+    }
+
+}
